8fd03f031f7f42ee9c537c3f9941bb32486fa5cb
AlexBeje <54982557+AlexBeje@users.noreply.github.com>
2022-03-04 10:22:14 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Hello Vue in CodeSandbox!" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
